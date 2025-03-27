fb835597921886b9209f404056d05543b1acb30a
Ludvigpbf <ludvig.flyckt@gmail.com>
2025-03-27 17:36:04 +0100

    feat: update import paths to use relative references and add logger utility for improved logging functionality

--- templates/js-templates/mvc/server.js
+++ templates/js-templates/mvc/server.js
@@ -1,12 +1,11 @@
-import express from 'express';
-import mainRouter from '@routes/mainRouter';
+import express from "express";
+import mainRouter from "./routes/mainRouter.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/api/v1', mainRouter);
+app.get("/api/v1", mainRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
- 
